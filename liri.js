a2b931c12cb0c71d9b73285de28aed97d09fca11
Esar Behlum <esarnb@gmail.com>
2019-06-17 14:39:28 -0700

    Initial LIRI files

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+/*
+    Packages
+*/
+
+/*
+    Global Variables
+*/
+
+var axios = require("axios");
+var keys = require("./keys.js")
+var spotify = new Spotify(keys.spotify)
+
+/*
+    Functions
+*/
+
